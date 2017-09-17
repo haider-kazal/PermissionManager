b2e1ab32c047a64dd7333e66526b62e4b72e3811
Haider Ali Kazal <haider.ali@gagagugu.com>
2017-09-17 20:02:09 +0600

    Initial Commit

---
+++ PermissionManager/PermissionManager.h
@@ -0,0 +1,19 @@
+//
+//  PermissionManager.h
+//  PermissionManager
+//
+//  Created by Haider Ali Kazal on 9/17/17.
+//  Copyright © 2017 Haider Ali Kazal. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for PermissionManager.
+FOUNDATION_EXPORT double PermissionManagerVersionNumber;
+
+//! Project version string for PermissionManager.
+FOUNDATION_EXPORT const unsigned char PermissionManagerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PermissionManager/PublicHeader.h>
+
+
